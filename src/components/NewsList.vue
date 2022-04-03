852c16d19e3126f133d47709c52b2143b570ff9d
Rupert Scott <rupertscott1706@gmail.com>
2022-04-02 21:13:40 -0500

    Added NewsList.vue

---
+++ src/components/NewsList.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return { };
+    },
+};
+</script>
+
+<template>
+    <ul class="news__list">
+        <li class="news__item">News item 1</li>
+        <li class="news__item">News item 2</li>
+        <li class="news__item">News item 3</li>
+    </ul>
+</template>
+
