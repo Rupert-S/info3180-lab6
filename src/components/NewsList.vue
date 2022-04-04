ad70e0dc85be04ce7023c0e71ee505375b9f6e3a
Rupert Scott <rupertscott1706@gmail.com>
2022-04-03 21:52:45 -0500

    Added CSS

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,16 +1,47 @@
-<script>
-export default {
-    data() {
-        return { };
-    },
-};
-</script>
-
 <template>
     <ul class="news__list">
-        <li class="news__item">News item 1</li>
-        <li class="news__item">News item 2</li>
-        <li class="news__item">News item 3</li>
+        <div v-for="article in articles" class="news__item">
+            <img :src=article.urlToImage width = "200" height = "200" />
+            <p >{{ article.title }} </p>
+            <p> {{ article.description }} </p>
+        </div>
     </ul>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            articles: []
+        };
+    },
+    created() {
+        let self = this;
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+    {
+        headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        self.articles = data.articles
+    });
+    }
+};
+</script>
+<style>
+.news__list{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+    
+}
+.news__item{
+    border-bottom: solid blue;
+}
+
+</style>
